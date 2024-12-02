e66430e541b2e378e651d850f386f14159632969
user <user@WINDOWS-5AN5BRH>
2024-12-02 20:12:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a = 1, b = 2, c = 3; // Задаємо коефіцієнти
+        Func<double, double> quadraticFunc = x => a * x * x + b * x + c;
+
+        for (int x = 0; x <= 10; x++)
+        {
+            double y = quadraticFunc(x);
+            Console.WriteLine($"y({x}) = {y}");
+        }
+    }
+}
+
